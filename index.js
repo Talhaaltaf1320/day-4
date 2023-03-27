e2342fbd4e33f7b32085d593122f700d4b6f6f08
Talhaaltaf1320 <127618327+Talhaaltaf1320@users.noreply.github.com>
2023-03-27 21:57:08 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+alert("hi talha");
+var b =confirm("are you talha");
+console.log(b)
+var a =prompt("enter your age");
+alert("Dear your age is" + a);
+console.log("Dear your age is" + a);
+document.write("Dear your age is" + a);
